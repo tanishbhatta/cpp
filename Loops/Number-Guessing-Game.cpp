54a7dcf04fad2e8cf679079ffddcc3f3baefc114
Tanish Bhatta <tanishbhattaai@gmail.com>
2026-09-02 23:54:45 +0545

    feat: added random library

--- Loops/Number-Guessing-Game.cpp
+++ Loops/Number-Guessing-Game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <string>
 using namespace std;
 
 int main(){
@@ -19,9 +20,26 @@ int main(){
     uniform_int_distribution<int> distrib(low, high);
     int num = distrib(gen);
 
-    int count = 0;
-    while (count < atm){
-        
+    int count = 1;
+    while (count <= atm){
+        int guess;
+        cout << "\nGuess a number: ";
+        cin >> guess;
+
+        string warn;
+        if(count == atm){
+            warn = to_string(cin);
+        }
+
+        if (guess == num){
+            cout << "\nYou've guessed the number in " << count << " attempts.";
+            break;
+        }else if(guess > num){
+            cout << "The number is lower." << endl;
+        }else{
+            cout << "The number is higher." << endl;
+        }
+        count += 1;
     };
 
 };
