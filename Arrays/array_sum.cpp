48420de734856d612842355ff70519237c0bbf14
Tanish Bhatta <tanishbhattaai@gmail.com>
2026-09-25 15:44:04 +0545

    feat: array_sum.cpp completed with buffer intake

--- Arrays/array_sum.cpp
+++ Arrays/array_sum.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
+int arrSum(int arr[], int size){
+    int res{};
+    for (int i=0; i<size; i++){
+        res += arr[i];
+    }
+    return res;
+}
+
 int main() {
     int n{};
-    int arr[n]{};
     std::cin >> n;
-    
-    for (int i=0; i<n; i++){
-        int item;
+    int arr[n];
+
+    for (int i = 0; i<n; i++){
+        int item{};
         std::cin >> item;
+        arr[i] = item;
     }
 
+    std::cout << arrSum(arr, n) << std::endl;
     return 0;
 }
