0b847aba0050e0dffdeda1f02b94dafaf96f7bc5
Tanish Bhatta <tanishbhattaai@gmail.com>
2026-09-26 23:21:19 +0545

    feat(strings) modified anagram_check
    applied one only array to the solution removing the second and correspondingly added and subtracted the values in every iteration of character and its position.

--- Strings/anagram_check.cpp
+++ Strings/anagram_check.cpp
@@ -2,26 +2,26 @@
 #include <string>
 
 bool isAnagram(const std::string &s, const std::string &w){
-    int arrS[26]{}, arrW[26]{};
+    if (s.length() != w.length()) return false;
+    int arr[26]{};
 
-    for (char c : s){
-        arrS[c - 'a']++;
+    for (char sc : s){
+        arr[sc - 'a']++;
     }
 
-    for (char c : w){
-        arrW[c - 'a']++;
+    for (char wc : w){
+        arr[wc - 'a']--;
     }
 
-    for (int i = 0; i<26; ++i){
-        if (arrS[i] != arrW[i]) return false;
+    for (int i=0; i<26; i++){
+        if (arr[i] != 0) return false;
     }
     return true;
 }
 
 int main() {
     std::string s{}, w{};
-    std::cin >> s;
-    std::cin >> w;
+    std::cin >> s; std::cin >> w;
 
     if (isAnagram(s, w)) std::cout << "YES" << std::endl;
     else std::cout << "NO" << std::endl;
